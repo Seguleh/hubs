561a77ebcee43cd7d95584f1b3ff8a442927beea
Andres Zuñiga <aezhco@gmail.com>
2019-02-22 11:50:29 -0300

    More rspec tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "Get index" do
-    it "renders the index action" do
+    it "renders the index view" do
       get :index
       expect(response).to render_template("index")
     end
@@ -16,35 +16,44 @@ RSpec.describe HomeController, type: :controller do
   end
   
   describe "Get search" do
-    it "renders the index action" do
+    it "renders the search view" do
       get :search
       expect(response).to render_template("search")
     end
   end
 
   describe "Get find" do
-    it "renders the index action" do
+    it "renders the find view" do
       get :find
       expect(response).to render_template("find")
     end
   end
 
+  let(:buffer) {StringIO.new}
+  let(:zip) {Rails.root.to_s + "/spec/fixtures/files/file.zip"}
+
   describe "File & data handling" do
-    it "should open file" do
-
+    it "should open the file correctly" do
+      File.open(zip) do |f|
+        expect(f).to be_a File
+      end
     end
 
-    it "should handle only zip file type" do
-
+    it "should recieve data from file" do
+      allow(File).to receive(:open).with(zip, 'wb').and_yield(buffer)
     end
 
-    it "reads data correctly" do
-
+    it "should not accept empty files" do
+      expect(File.size(zip)).to be > 0
     end
 
-    it "converts data correctly" do
-      
+    it "should only be zip file type" do
+      type = MIME::Types.type_for(zip).first.content_type
+      expect(type).to eq("application/zip")
+    end
+
+    it "reads at least some data from the file" do
+      expect(File.read(zip)).not_to be_nil
     end
   end
-
 end
